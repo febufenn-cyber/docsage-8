fc46d8fabc2d7b70a05a1ec345cb9974b3b95e7e
febufenn-cyber <febufenn@gmail.com>
2026-07-14 11:16:32 +0530

    Export Phase 3 learning contracts

---
+++ packages/learning/src/index.mjs
@@ -0,0 +1,9 @@
+export {
+  LEARNING_EVENT_TYPES,
+  LEARNING_SOURCES,
+  normalizeLearningMetadata,
+  normalizeLearningEvent,
+  canonicalLearningEvent
+} from './contracts.mjs';
+export { normalizeQuestionText, redactSensitiveText, fingerprintQuestion } from './privacy.mjs';
+export { MemoryLearningEventStore, LearningEventConflictError } from './store.mjs';
